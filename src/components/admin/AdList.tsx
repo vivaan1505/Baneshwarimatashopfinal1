2987132438fe82e910ef7254f25c8951e51779d7
Vipin Gehlot <vipin.gehlot1505@gmail.com>
2025-06-07 15:53:33 -0300

    Updated AdList.tsx

--- src/components/admin/AdList.tsx
+++ src/components/admin/AdList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../utils/supabaseClient';
+import { supabase } from '../../lib/supabase';
 import { Ad } from '../../types/ad';
 import AdForm from './AdForm';
 
