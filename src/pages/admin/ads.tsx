051019f02e0dca19285de162a4c5425c1a7caa23
Vipin Gehlot <vipin.gehlot1505@gmail.com>
2025-06-07 14:03:24 -0300

    Updated ads.tsx

--- src/pages/admin/ads.tsx
+++ src/pages/admin/ads.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import AdList from '../../components/admin/AdList';
 
 const AdminAdsPage: React.FC = () => (
-  <div className="max-w-5xl mx-auto p-4 md:p-8">
-    <h1 className="text-2xl font-bold mb-6">Ad & Script Management</h1>
+  <div>
+    <h1>Ad Management</h1>
     <AdList />
   </div>
 );
